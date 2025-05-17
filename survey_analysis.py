285720ffd571c3a9d8236106bba0e2a8af01577e
dante.am <dante.avino@alum.esci.upf.edu>
2025-05-17 11:40:35 +0200

    report

--- survey_analysis.py
+++ survey_analysis.py
@@ -2,5 +2,14 @@
 import pandas as pd
 
 # %%
-df_survey = pd.read_csv("data/Log_Survey_Translated.csv")
+df_survey = pd.read_csv("data/Log_Survey_Translated.csv", index_col=0)
+# %%
+
+from ydata_profiling import ProfileReport
+
+profile = ProfileReport(
+    df_survey, title="VisualPanorama - Survey Results", explorative=True, minimal=False
+)
+profile.to_file("interactive_survey_report_full.html")
+
 # %%
